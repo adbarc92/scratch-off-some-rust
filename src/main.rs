f9707aee4458cff3dcab549e80a663294140c669
Alex Barclay <adbarclay92@gmail.com>
2024-04-14 10:12:20 -0600

    First practice with modules

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
-async fn make_request() -> Result<(), Box<dyn std::error::Error>> {
-    // Specify the URL you want to send the GET request to
-    let url = "https://google.com";
-
-    // Send the GET request and await the response
-    let response = reqwest::get(url).await?;
-
-    // Check if the request was successful (status code 200)
-    if response.status().is_success() {
-        // Get the response body as bytes
-        let body = response.bytes().await?;
-
-        // Convert the bytes into a string (assuming it's UTF-8 encoded)
-        let body_as_string = String::from_utf8(body.to_vec())?;
-
-        // Print the response body
-        println!("{}", body_as_string);
-    } else {
-        // Print an error message if the request was not successful
-        println!("Request failed with status code: {}", response.status());
-    }
-
-    Ok(())
-}
+mod request_helpers;
+use request_helpers::request_helpers::make_request;
 
 #[tokio::main]
 async fn main() {
